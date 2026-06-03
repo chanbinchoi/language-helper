029b5e972bb7ce74dc4d0e185b6b92da773996e4
chanbinchoi <classy9611@gmail.com>
2026-06-04 07:02:57 +0900

    first commit

---
+++ src/main/java/com/languagehelper/LanguageHelperApplication.java
@@ -0,0 +1,13 @@
+package com.languagehelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanguageHelperApplication.class, args);
+    }
+
+}
